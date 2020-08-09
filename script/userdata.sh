b201bd703774b0da9634dc9ff2260848501c9235
ebi-exp <ebi.exp@gmail.com>
2020-08-09 15:37:34 +0900

    [fix] userdata files

--- script/userdata.sh
+++ script/userdata.sh
@@ -7,10 +7,8 @@ yum -y update
 yum install -y git  \
                 golang
 
-# download project files
+# add project files
 cd /home/ec2-user
-git clone https://github.com/ebi-yade/crawler # clone this repo
-#aws s3 cp s3://crawler-ebi-dev/dotfiles/.env /crawler/.env # get .env on s3 with default encription
 
 # change owner of project directory
 chown -R ec2-user:ec2-user ./crawler
